d2fa2b4a813b99a50aca16d59336332cac0488ba
dhafin <dhio@dhaf.in>
2025-01-27 06:17:28 +0700

    feat: header style

--- src/components/sharedUI/Header.tsx
+++ src/components/sharedUI/Header.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Header = () => {
-  return <div>Todolist</div>;
+  return (
+    <div className="mb-4 w-full items-center flex justify-center">
+      <h1 className="text-3xl font-medium">Ordinary Todolists</h1>
+    </div>
+  );
 };
